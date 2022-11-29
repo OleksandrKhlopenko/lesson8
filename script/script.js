32a27294350138722cc1dd6f9d8318525867bf9c
OleksandrKhlopenko <108742474+OleksandrKhlopenko@users.noreply.github.com>
2022-11-29 19:33:05 +0200

    lesson8

---
+++ script/script.js
@@ -0,0 +1,26 @@
+"use strict"
+
+class Animal {
+
+    constructor(name) {
+      this.name = name;
+    }
+  
+  }
+  
+  class Rabbit extends Animal {
+    constructor(name) {
+     super(name)
+      this.name = name;
+      this.created = Date.now();
+    }
+  }
+  
+  let rabbit = new Rabbit("White rabbit"); 
+  alert(rabbit.name);
+
+
+
+
+
+
